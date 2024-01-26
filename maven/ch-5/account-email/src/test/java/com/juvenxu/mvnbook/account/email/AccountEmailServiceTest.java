a014ae0cdbd63322a59308f0a47475a9349b0567
mark.lin <mark.lin@yuguo.com>
2024-01-26 15:13:12 +0800

    no message

---
+++ maven/ch-5/account-email/src/test/java/com/juvenxu/mvnbook/account/email/AccountEmailServiceTest.java
@@ -0,0 +1,60 @@
+package com.juvenxu.mvnbook.account.email;
+
+import static junit.framework.Assert.assertEquals;
+
+import javax.mail.Message;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetup;
+
+
+public class AccountEmailServiceTest {
+
+    /*email.protocol=smtp
+    email.host=localhost
+    email.port=25
+    email.username=test@juvenxu.com
+    email.password=123456
+    email.auth=true
+    email.systemEmail=test@juvenxu.com*/
+
+    private GreenMail greenMail;
+
+    @Before
+    public void startMailServer() throws Exception {
+        greenMail = new GreenMail(ServerSetup.SMTP);
+        greenMail.setUser("test@juvenxu.com", "123456");
+        greenMail.start();
+    }
+
+    @Test
+    public void testSendMail() throws Exception {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("account-email.xml");
+
+        //通过Spring的IOC获得AccountEmailService的实现类对象
+        AccountEmailService accountEmailService = (AccountEmailService) ctx.getBean("accountEmailService");
+        String subject = "Test Subject";
+        String htmlText = "<h3>Test</h3>";
+        accountEmailService.sendMail("test2@juvenxu.com", subject, htmlText);
+        //
+        greenMail.waitForIncomingEmail(2000, 1);
+
+        Message[] msgs = greenMail.getReceivedMessages();
+        assertEquals(1, msgs.length);
+        assertEquals(subject, msgs[0].getSubject());
+        assertEquals(htmlText, GreenMailUtil.getBody(msgs[0]).trim());
+    }
+
+    @After
+    public void stopMailServer() throws Exception {
+        greenMail.stop();
+    }
+}
